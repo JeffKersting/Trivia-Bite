8d86d07a73621555deaea1c4b03df0d317cf64d0
Jeff Kersting <jeff.w.kersting@gmail.com>
2021-06-04 21:58:18 -0600

    Create table relationship

--- db/knexfile.js
+++ db/knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
   development: {
     client: 'postgresql',
     connection: {
-      database: 'officeTriva',
+      database: 'officetrivia',
     },
     pool: {
       min: 2,
--- db/migrations/20210605033719_init.js
+++ db/migrations/20210605033719_init.js
@@ -1,16 +1,17 @@
 
 exports.up = function(knex) {
   return knex.schema
+    .createTable('groups', table => {
+      table.increments('id')
+      table.string('group_name').notNullable().unique()
+    })
     .createTable('users', table => {
       table.increments('id').primary()
       table.string('name').notNullable().unique()
       table.string('email').notNullable().unique()
+      table.string('groups').unsigned()
+      table.foreign('groups').references('groups.group_name')
       table.timestamps(true, true)
-    })
-    .createTable('groups', table => {
-      table.increments('id').primary()
-      table.string('group_name').notNullable().unique()
-      table.foreign('members').references('users.id')
     })
 };
 
