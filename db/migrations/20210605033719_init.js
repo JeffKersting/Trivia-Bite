bc1e48ed2ddb5688d83a7e13afec2d4a6f16a9d4
Jeff Kersting <jeff.w.kersting@gmail.com>
2021-06-07 17:45:06 -0600

    Update user/group tables. Add questions table

--- db/migrations/20210605033719_init.js
+++ db/migrations/20210605033719_init.js
@@ -1,16 +1,27 @@
 
 exports.up = function(knex) {
   return knex.schema
-    .createTable('groups', table => {
-      table.increments('id')
-      table.string('group_name').notNullable().unique()
-    })
     .createTable('users', table => {
       table.increments('id').primary()
       table.string('name').notNullable().unique()
       table.string('email').notNullable().unique()
-      table.string('groups').unsigned()
-      table.foreign('groups').references('groups.group_name')
+      table.timestamps(true, true)
+    })
+    .createTable('groups', table => {
+      table.increments('user_id').unsigned()
+      table.foreign('user_id').references('users.id')
+      table.integer('daily_score')
+      table.string('group_name')
+      table.timestamps(true, true)
+    })
+    .createTable('questions', table => {
+      table.increments('id').primary()
+      table.string('category')
+      table.string('question')
+      table.string('correct_answer')
+      table.string('incorrect_1')
+      table.string('incorrect_2')
+      table.string('incorrect_3')
       table.timestamps(true, true)
     })
 };
