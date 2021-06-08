51ead9c3de6bf3f019591fd7af39d6abea29d4e9
Jeff Kersting <jeff.w.kersting@gmail.com>
2021-06-08 15:44:43 -0600

    Update questions service to add all new questions

--- cron-jobs/questions.js
+++ cron-jobs/questions.js
@@ -4,17 +4,21 @@ const fetch = require('node-fetch')
 
 const getQuestions = async () => {
   try {
-    const questions = await fetch('https://opentdb.com/api.php?amount=1&type=multiple').then(response => response.json())
-    const {category, question, correct_answer, incorrect_answers} = questions.results[0]
+    const questionsDto = []
+    const questions = await fetch('https://opentdb.com/api.php?amount=10&type=multiple').then(response => response.json())
+    questions.results.forEach(questionObj => {
 
-    const questionsDto = {
-      category: category,
-      question: question,
-      correct: correct_answer,
-      incorrect1: incorrect_answers[0],
-      incorrect2: incorrect_answers[1],
-      incorrect3: incorrect_answers[2]
-    }
+      const {category, question, correct_answer, incorrect_answers} = questionObj
+      const formattedQuestion = {
+          category: category,
+          question: question,
+          correct: correct_answer,
+          incorrect1: incorrect_answers[0],
+          incorrect2: incorrect_answers[1],
+          incorrect3: incorrect_answers[2]
+        }
+      questionsDto.push(formattedQuestion)
+    })
 
     questionsService.createQuestions(questionsDto)
   } catch (err) {
--- service/questions.js
+++ service/questions.js
@@ -8,14 +8,14 @@ const clearQuestions = async () => {
 
 class QuestionsService {
   createQuestions = async (questionsDto) => {
-    const deleted = await clearQuestions()
-    if (deleted) {
-      const {category, question, correct, incorrect1, incorrect2, incorrect3} = questionsDto
-      return questionsDAO.createQuestions(category, question, correct, incorrect1, incorrect2, incorrect3)  
-    }
-
-
+    const deleted = await clearQuestions().then(
+      questionsDto.forEach(questionObject => {
+        const {category, question, correct, incorrect1, incorrect2, incorrect3} = questionObject
+        return questionsDAO.createQuestions(category, question, correct, incorrect1, incorrect2, incorrect3)
+      })
+    )
   }
 }
+
 
 module.exports = new QuestionsService()
