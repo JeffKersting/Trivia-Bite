0cb497266906cb76e9f5e01290d06f19408bb2e8
Jeff Kersting <jeff.w.kersting@gmail.com>
2021-06-07 20:41:50 -0600

    Install node-cron. Setup simple console.log

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express')
+const cron = require('node-cron')
 const router = require('./routes')
+
 
 const app = express()
 app.use(express.json())
 app.use(router)
+
+// cron.schedule('0 0 24 * * *') 24 HOUR SETUP
+cron.schedule('* * * * * ', () => {
+  console.log('Scheduler running...')
+})
 
 app.listen(8080, () => console.log('server listening on port 8080'))
