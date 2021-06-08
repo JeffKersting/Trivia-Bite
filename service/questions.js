e25ca7535c199901ecb00f171639c79ddeabe1df
Jeff Kersting <jeff.w.kersting@gmail.com>
2021-06-08 15:50:39 -0600

    Cleanup service syntax

--- service/questions.js
+++ service/questions.js
@@ -7,8 +7,8 @@ const clearQuestions = async () => {
 }
 
 class QuestionsService {
-  createQuestions = async (questionsDto) => {
-    const deleted = await clearQuestions().then(
+  createQuestions = (questionsDto) => {
+    clearQuestions().then(
       questionsDto.forEach(questionObject => {
         const {category, question, correct, incorrect1, incorrect2, incorrect3} = questionObject
         return questionsDAO.createQuestions(category, question, correct, incorrect1, incorrect2, incorrect3)
