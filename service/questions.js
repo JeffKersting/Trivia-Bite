a07508bfcd37088829ab5a7b474e498974ee2018
Jeff Kersting <jeff.w.kersting@gmail.com>
2021-06-08 15:12:45 -0600

    Add table clearing before data replacement

--- service/questions.js
+++ service/questions.js
@@ -1,9 +1,20 @@
 const questionsDAO = require('../dao/questions')
+const db = require('../db/db')
+
+const clearQuestions = async () => {
+  const deleted = await db('questions').del()
+  return deleted
+}
 
 class QuestionsService {
-  createQuestions(questionsDto) {
-    const {category, question, correct, incorrect1, incorrect2, incorrect3} = questionsDto
-    return questionsDAO.createQuestions(category, question, correct, incorrect1, incorrect2, incorrect3)
+  createQuestions = async (questionsDto) => {
+    const deleted = await clearQuestions()
+    if (deleted) {
+      const {category, question, correct, incorrect1, incorrect2, incorrect3} = questionsDto
+      return questionsDAO.createQuestions(category, question, correct, incorrect1, incorrect2, incorrect3)  
+    }
+
+
   }
 }
 
