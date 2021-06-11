e05ef7694d9f55635b807f25f90b51165c5dc6a4
Jeff Kersting <jeff.w.kersting@gmail.com>
2021-06-11 12:31:35 -0600

    Create axios create user post

--- client/src/components/login/Login.js
+++ client/src/components/login/Login.js
@@ -1,23 +1,33 @@
 import { useState } from 'react'
 import { GoogleLogin } from 'react-google-login'
+import axios from 'axios'
 
 function Login() {
 
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
-  const [url, setUrl] = useState('')
+  const [token, setToken] = useState('')
 
   const responseGoogle = (response) => {
-    console.log(response.qc.id_token)
-    setEmail(response.profileObj.email)
     setName(response.profileObj.name)
+    setEmail(response.profileObj.email)
+    setToken(response.qc.id_token)
+    axios({
+      method: 'POST',
+      url: 'http://localhost:8080/user',
+      data: {
+        name: response.profileObj.name,
+        email: response.profileObj.email,
+        token: response.qc.id_token
+      }
+    })
   }
 
   return (
     <>
       <GoogleLogin
         clientId="764899056810-2d6oki589hnrp6ir2i1pd1f3o41tg732.apps.googleusercontent.com"
-        buttonText="Login"
+        buttonText="Login with Google"
         onSuccess={responseGoogle}
         onFailure={responseGoogle}
         cookiePolicy={'single_host_origin'}
