3ad1e0a28a0eb6bfbc80ecd10ebfc156c2805012
Jeff Kersting <jeff.w.kersting@gmail.com>
2021-06-15 15:07:01 -0600

    Add member folder/component to sidebar folder

--- client/src/components/sidebar/Sidebar.js
+++ client/src/components/sidebar/Sidebar.js
@@ -1,19 +1,17 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import axios from 'axios'
+import GroupMembers from './group-members/Group-Members'
 
 function Sidebar() {
+  const [group, setGroup] = useState(null)
+
   const getGroup = async () => {
-    const group = []
     const groupData = await axios.get('http://localhost:8080/group', {
       params: {
         groupId: '1'
       }
     })
-    console.log(groupData)
-    // groupData.data.forEach(member => {
-    //   group.push({name: member.name, score: member.daily_score})
-    // })
-    // setGroup(group)
+    setGroup(groupData.data)
   }
 
   useEffect(() => {
@@ -21,7 +19,10 @@ function Sidebar() {
   }, [])
 
   return (
-    <div>Sidebar</div>
+    <>
+      <div>Sidebar</div>
+      { group && group.map((member, index) => <GroupMembers member={member} key={index}/>) }
+    </>
   )
 }
 
