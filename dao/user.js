708965a30dfec2ea95751a68fd50a0008d1eee5d
Jeff Kersting <jeff.w.kersting@gmail.com>
2021-06-17 12:58:36 -0600

    Add updateUserScore method to user DAO

--- dao/user.js
+++ dao/user.js
@@ -19,6 +19,16 @@ class UserDAO {
       console.log(err)
     }
   }
+
+  async updateUserScore(userId, score) {
+    try {
+      const score = await db('users').where({ id: userId }).update({
+        daily_score: score
+      }).returning('daily_score')
+    } catch(err) {
+      console.log(err)
+    }
+  }
 }
 
 module.exports = new UserDAO()
