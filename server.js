546ef3fa85faba35b62cb66ffc958a34e3e322ef
Jeff Kersting <jeff.w.kersting@gmail.com>
2021-06-23 16:51:56 -0600

    Complete reset cron job and add to cron schedule

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cron = require('node-cron')
 const router = require('./routes')
 const cors = require('cors')
 const getQuestions = require('./cron-jobs/questions')
+const resetDailyScore = require('./cron-jobs/reset-scores')
 
 const app = express()
 app.use(cors())
@@ -11,6 +12,7 @@ app.use(router)
 
 cron.schedule('0 1 * * * ', () => {
   getQuestions()
+  resetDailyScore()
   console.log('Fetching questions...')
 })
 
