35484571782cad0a9bc4f3620597045007bc5e1a
Jeff Kersting <jeff.w.kersting@gmail.com>
2021-06-23 11:08:51 -0600

    Remove unnecessary React imports from Quiz.js

--- client/src/components/quiz/Quiz.js
+++ client/src/components/quiz/Quiz.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef, useCallback } from 'react'
+import { useState, useEffect } from 'react'
 import Question from '../question/Question'
 import Timer from './timer/Timer'
 
