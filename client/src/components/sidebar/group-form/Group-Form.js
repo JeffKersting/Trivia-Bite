a66fd0e0678c288ee9e4cec9e80982e3478415a9
Jeff Kersting <jeff.w.kersting@gmail.com>
2021-06-25 15:06:18 -0600

    Add GroupForm component with input

--- client/src/components/sidebar/group-form/Group-Form.js
+++ client/src/components/sidebar/group-form/Group-Form.js
@@ -1,5 +1,17 @@
 import { useState } from 'react'
 
 function GroupForm() {
-  
+  const [groupInput, setInput] = useState('')
+
+  return (
+    <form>
+      <input
+        type='text'
+        placeholder='Group Name...'
+        value={groupInput}
+        autoComplete='off'
+        onChange={inputHandler}
+      />
+    </form>
+  )
 }
