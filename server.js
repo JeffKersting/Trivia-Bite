46d711768d755ffbd7a0e8bf80a857d66b3f3a7a
Jeff Kersting <jeff.w.kersting@gmail.com>
2021-07-05 13:39:38 -0600

    Remove express file path routing

--- server.js
+++ server.js
@@ -20,13 +20,13 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use('/', router)
 
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build/index.html'), (err) => {
-    if (err) {
-      res.status(500).send(err)
-    }
-  })
-})
+// app.get('/*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'client/build/index.html'), (err) => {
+//     if (err) {
+//       res.status(500).send(err)
+//     }
+//   })
+// })
 
 
 if (process.env.NODE_ENV === 'production') {
