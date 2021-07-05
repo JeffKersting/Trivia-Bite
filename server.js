7764f9bdb0a99b0fbff3b96495b1a163084292d9
Jeff Kersting <jeff.w.kersting@gmail.com>
2021-07-05 10:27:43 -0600

    Setup static file serving for react

--- server.js
+++ server.js
@@ -4,11 +4,20 @@ const router = require('./routes')
 const cors = require('cors')
 const getQuestions = require('./cron-jobs/questions')
 const resetDailyScore = require('./cron-jobs/reset-scores')
+const port = process.env.PORT || 8080
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(router)
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+  })
+}
 
 cron.schedule('0 1 * * * ', () => {
   getQuestions()
@@ -16,4 +25,4 @@ cron.schedule('0 1 * * * ', () => {
   console.log('Fetching questions...')
 })
 
-app.listen(8080, () => console.log('server listening on port 8080'))
+app.listen(app.get('port'), () => console.log(`server listening on port ${port}`))
