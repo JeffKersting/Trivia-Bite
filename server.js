fb397fadab85d99cf73c906ca882fc0640713332
Jeff Kersting <jeff.w.kersting@gmail.com>
2021-07-05 12:07:39 -0600

    Add heroku procfile

--- server.js
+++ server.js
@@ -1,37 +1,36 @@
 const express = require('express')
-const path = require('path')
 const cron = require('node-cron')
 const router = require('./routes')
 const cors = require('cors')
+const helmet = require('helmet')
 const getQuestions = require('./cron-jobs/questions')
 const resetDailyScore = require('./cron-jobs/reset-scores')
 const port = process.env.PORT || 8080
 const whitelist = ['http://localhost:3000', 'http://localhost:8080', /*ADD HEROKU URL*/]
+
 const app = express()
 
-const corsOptions = {
-  origin: (origin, callback) => {
-    console.log('** Origin of request' + origin)
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      console.log('Origin acceptable')
-      callback(null, true)
-    } else {
-      console.log('Origin rejected')
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
+// const corsOptions = {
+//   origin: (origin, callback) => {
+//     console.log('** Origin of request' + origin)
+//     if (whitelist.indexOf(origin) !== -1 || !origin) {
+//       console.log('Origin acceptable')
+//       callback(null, true)
+//     } else {
+//       console.log('Origin rejected')
+//       callback(new Error('Not allowed by CORS'))
+//     }
+//   }
+// }
 
+app.use(helmet())
 app.use(cors())
 app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 app.use(router)
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')))
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
-  })
+  app.use(express.static('client/build'))
 }
 
 cron.schedule('0 1 * * * ', () => {
@@ -40,4 +39,4 @@ cron.schedule('0 1 * * * ', () => {
   console.log('Fetching questions...')
 })
 
-app.listen(app.get('port'), () => console.log(`server listening on port ${port}`))
+app.listen(port, () => console.log(`server listening on port ${port}`))
