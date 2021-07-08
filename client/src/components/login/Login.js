ddc7555e3a3e218cbd400d7acacccc86d30d26f5
Jeff Kersting <jeff.w.kersting@gmail.com>
2021-07-07 21:29:13 -0600

    Add about site to login with styling

--- client/src/components/login/Login.js
+++ client/src/components/login/Login.js
@@ -19,15 +19,20 @@ function Login({ setUser, user }) {
         <p className='trivia'>Trivia</p>
         <p className='bite'>Bite</p>
       </div>
-      <GoogleLogin
-        className="google-login"
-        clientId="764899056810-2d6oki589hnrp6ir2i1pd1f3o41tg732.apps.googleusercontent.com"
-        buttonText="Login with Google"
-        onSuccess={responseGoogle}
-        onFailure={responseGoogle}
-        cookiePolicy={'single_host_origin'}
-        isSignedIn={true}
-      />
+      <div className='login-section'>
+        <p className='site-about'>
+          Welcome to Trivia Bite. Every day there are ten new trivia questions added. You can play alone, join a group a friend has made, or create your own group and invite friends to compete daily.
+        </p>
+        <GoogleLogin
+          className="google-login"
+          clientId="764899056810-2d6oki589hnrp6ir2i1pd1f3o41tg732.apps.googleusercontent.com"
+          buttonText="Login with Google"
+          onSuccess={responseGoogle}
+          onFailure={responseGoogle}
+          cookiePolicy={'single_host_origin'}
+          isSignedIn={true}
+        />
+      </div>
     </div>
   )
 }
