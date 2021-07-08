0156f0aa7a3815ab69023f91a834e9d96a0cfb50
Jeff Kersting <jeff.w.kersting@gmail.com>
2021-07-08 11:48:13 -0600

    Add background gradient to document body. Add site title to loading screen

--- client/src/components/loading/Loading.js
+++ client/src/components/loading/Loading.js
@@ -2,6 +2,11 @@ function Loading() {
 
   return (
     <>
+    <div className='site-name'>
+      <p className='trivia'>Trivia</p>
+      <p className='bite'>Bite</p>
+      <div className='login-icon'/>
+    </div>
       <div className='loading'/>
     </>
   )
