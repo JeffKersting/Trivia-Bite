48026efe95a84ca5dd109eea975d0536c7838e8a
Jeff Kersting <jeff.w.kersting@gmail.com>
2021-07-12 10:46:52 -0600

    Replace ampersands parameter

--- cron-jobs/questions.js
+++ cron-jobs/questions.js
@@ -2,7 +2,7 @@ const questionsService = require('../service/questions')
 const fetch = require('node-fetch')
 
 const cleanupString = (string) => {
-  return string.replace(/&quot;/g, '').replace(/&#039;/g, '').replace(/&acute;/g, '').replace(/&reg;/g, '').replace(/&trade;/g, '')
+  return string.replace(/&quot;/g, '').replace(/&#039;/g, '').replace(/&acute;/g, '').replace(/&reg;/g, '').replace(/&trade;/g, '').replace(/&amp;/g, '&')
 }
 
 const getQuestions = async () => {
